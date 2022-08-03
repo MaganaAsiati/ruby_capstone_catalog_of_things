6fc68a04706121e2f285aa28cc9ab5b0e6813f47
Theophileaseh <theophilenwachan@gmail.com>
2022-08-03 11:01:43 +0100

    Create Game class and implement methods

---
+++ model/game.rb
@@ -0,0 +1,17 @@
+require 'date'
+require './item'
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :publish_date
+  attr_reader :id
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
+    @id = Random.rand(1..1000)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    super && (Date.today.year - @last_played_at.year) > 2
+  end
+end
