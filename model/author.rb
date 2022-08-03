e4eb35864da35afee359037fc04e411bc75ae221
Theophileaseh <theophilenwachan@gmail.com>
2022-08-03 10:33:22 +0100

    Add id

--- model/author.rb
+++ model/author.rb
@@ -3,6 +3,7 @@ class Author
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
