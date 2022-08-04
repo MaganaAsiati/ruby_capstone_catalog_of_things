e3ff710feac9fce90a9661731730a1ae0a02984d
Theophileaseh <theophilenwachan@gmail.com>
2022-08-04 10:18:26 +0100

    Solve item_spec.rb error

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,16 +2,16 @@ require_relative '../model/item'
 require 'date'
 
 describe Item do
-  before(:example) do
-    @instance = Item.new(publish_date: '2010-10-14')
-  end
+  context '... this class...' do
+    instance = Item.new('2010-10-14')
 
-  it 'should initialize' do
-    expect(@instance.publish_date).to eq(Date.parse('2010-10-14'))
-  end
+    it 'should initialize' do
+      expect(instance.publish_date).to eq(Date.parse('2010-10-14'))
+    end
 
-  it 'should move item to archive if item can be archived' do
-    @instance.move_to_archive
-    expect(@instance.archived).to be(true)
+    it 'should move item to archive if item can be archived' do
+      instance.move_to_archive
+      expect(instance.archived).to be(true)
+    end
   end
 end
