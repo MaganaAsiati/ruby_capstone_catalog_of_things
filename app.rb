37cc9b1dd031a3ab4fa32564b9797fb42987c295
Theophileaseh <theophilenwachan@gmail.com>
2022-08-04 21:59:37 +0100

    implement List authors, games and add games functionalities

--- app.rb
+++ app.rb
@@ -1,24 +1,49 @@
 require_relative './model/label'
+require_relative './model/author'
+require_relative './model/game'
 class App
-  attr_reader :genres, :authors, :labels
+  attr_reader :genres, :authors, :labels, :games
 
   def initialize
     @genres = []
     @authors = []
     @labels = []
+    @games = []
   end
 
   def populate_app
     labels = [Label.new(title: 'Drama', color: 'red'), Label.new(title: 'Fantasy', color: 'green')]
     labels.each { |label| @labels.push(label) }
+
+    # games = [Game.new(multiplayer: '30', last_played_at: '2022-01-11', publish_date: '1980-08-08'),
+    #          Game.new(multiplayer: '33', last_played_at: '2022-05-11', publish_date: '1980-08-08')]
+    # games.each { |game| @games.push(game) }
+
+    authors = [Author.new(first_name: 'name1', last_name: 'name2'), Author.new(first_name: 'name1', last_name: 'name2')]
+    authors.each { |author| @authors.push(author) }
   end
 
   def add_genre(item)
     # @genre.add_item(item)
   end
 
-  def add_author(item)
-    # @author.add_item(item)
+  # def add_author(item)
+  #   @author.add_item(item)
+  # end
+
+  def add_game
+    print 'Enter number of players: '
+    multiplayer = gets.chomp
+
+    print 'Enter Last Played Date format[yyyy-mm-dd]: '
+    last_played_at = gets.chomp
+
+    print 'Enter Date Published format[yyyy-mm-dd]: '
+    publish_date = gets.chomp
+
+    games = [Game.new(multiplayer, last_played_at, publish_date)]
+
+    games.each { |game| @games.push(game) }
   end
 
   # Label part
@@ -29,6 +54,19 @@ class App
   def list_labels
     @labels.each_with_index do |label, index|
       puts "#{index}. [#{label.class}] - Title: #{label.title}, Color: #{label.color}"
+    end
+  end
+
+  def list_games
+    @games.each_with_index do |game, index|
+      puts "#{index}. [#{game.class}] - Multiplayer: #{game.multiplayer}, Last Played Date: #{game.last_played_at},
+      Publish Date: #{game.publish_date}"
+    end
+  end
+
+  def list_authors
+    @authors.each_with_index do |author, index|
+      puts "#{index}. [#{author.class}] - First Name: #{author.first_name}, Last Name: #{author.last_name}"
     end
   end
 end
--- input.rb
+++ input.rb
@@ -21,27 +21,30 @@ class Input
     when 2
       #  List all music albums
     when 3
-      #  List of games
+      puts 'List all games'
+      app.list_games
     when 4
       #  List all genres
     when 5
       puts 'List all labels'
       app.list_labels
     when 6
-      #  List all authors
+      puts 'List all authors'
+      app.list_authors
     else
       puts 'invalid input'
     end
   end
 
-  def self.add_data(input, _app)
+  def self.add_data(input, apps)
     case input
     when 7
       #  Add a book
     when 8
       #  Add a music album
     when 9
-      #  Add a game
+      puts 'Add Game'
+      apps.add_game
     else
       puts 'invalid input'
     end
