c689918d22dc8f72294086d1b0de1970ecad5de8
MaganaAsiati <assia.magana@gmail.com>
2022-08-05 16:46:32 +0200

    prepare the storage

--- storage.rb
+++ storage.rb
@@ -6,4 +6,12 @@ class Storage
   def initialize
     @files = %w[books.json games.json albums.json]
   end
+
+  def prepare_storage
+    @files.each do |filename|
+      File.new(filename, 'w+') unless File.file?(filename)
+    end
+  end
+
+  
 end
