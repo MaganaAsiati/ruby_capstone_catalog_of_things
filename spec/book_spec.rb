241c1c82f01ff0b152549ccae5cd681dcf1082b6
MaganaAsiati <assia.magana@gmail.com>
2022-08-05 12:32:22 +0200

    test for the methode can_be_archived?

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,5 +13,12 @@ describe Book do
     expect(@book.cover_state).to eq('bad')
   end
 
-  
+  it 'should return false' do
+    expect(@book.archived).to be(false)
+  end
+
+  it 'should return true' do
+    @book.move_to_archive
+    expect(@book.archived).to be(true)
+  end
 end
