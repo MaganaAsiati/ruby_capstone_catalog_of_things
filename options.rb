dd90a902fa140b2af5bfe1e948486fe288e3ec11
Theophileaseh <theophilenwachan@gmail.com>
2022-08-05 20:57:19 +0100

    Fix linter errors

--- options.rb
+++ options.rb
@@ -1,18 +1,17 @@
-
 def options(app)
-loop do
-  puts 'Welcome to : Catalog of my things ! :)'
-  puts '1 - List all books'
-  puts '2 - List all music albums'
-  puts '3 - List of games'
-  puts '4 - List all genres'
-  puts '5 - List all labels'
-  puts '6 - List all authors'
-  puts '7 - Add a book'
-  puts '8 - Add a music album'
-  puts '9 - Add a game'
-  puts '0 - Exit'
-  input = gets.chomp.to_i
-  Input.check_input(input, app)
-end
+  loop do
+    puts 'Welcome to : Catalog of my things ! :)'
+    puts '1 - List all books'
+    puts '2 - List all music albums'
+    puts '3 - List of games'
+    puts '4 - List all genres'
+    puts '5 - List all labels'
+    puts '6 - List all authors'
+    puts '7 - Add a book'
+    puts '8 - Add a music album'
+    puts '9 - Add a game'
+    puts '0 - Exit'
+    input = gets.chomp.to_i
+    Input.check_input(input, app)
+  end
 end
