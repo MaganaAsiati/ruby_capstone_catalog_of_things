b051dccda4661944c4343ab3a21f7527ef152a71
Theophileaseh <theophilenwachan@gmail.com>
2022-08-05 17:59:40 +0100

    Update add game functionality

--- model/game.rb
+++ model/game.rb
@@ -11,6 +11,16 @@ class Game < Item
     @last_played_at = Date.parse(last_played_at)
   end
 
+  def to_json(*_args)
+    JSON.dump({
+                multiplayer: @multiplayer,
+                last_played_at: @last_played_at,
+                publish_date: @publish_date
+              })
+  end
+
+  private
+
   def can_be_archived?
     super && (Date.today.year - @last_played_at.year) > 2
   end
