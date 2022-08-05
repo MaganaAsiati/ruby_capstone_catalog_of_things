64f5fb6c27ef4d5ea55610f8a5f18bd85caac3e9
Theophileaseh <theophilenwachan@gmail.com>
2022-08-05 16:57:25 +0100

    Update files

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App
     @games << Game.new(multiplayer: multiplayer, last_played_at: last_played_at, publish_date: publish_date)
   end
 
+
   def add_game
     print 'Enter number of players: '
     multiplayer = gets.chomp
@@ -41,9 +42,9 @@ class App
     print 'Enter Date Published format[yyyy-mm-dd]: '
     publish_date = gets.chomp
 
-    games = [Game.new(multiplayer:, last_played_at:, publish_date:)]
+    # games = [Game.new(multiplayer:, last_played_at:, publish_date:)]
 
-    games.each { |game| @games.push(game) }
+    # games.each { |game| @games.push(game) }
   end
 
   # Label part
